05ce89fd65fb5d194ee9435b3edadedb35de0864
Maxim Kurin <kurinmaksim42@gmail.com>
2026-03-09 02:09:21 +0000

    disable animations

--- config/.config/lazyvim/lua/plugins/snacks.lua
+++ config/.config/lazyvim/lua/plugins/snacks.lua
@@ -2,6 +2,9 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      animate = { enabled = false },
+      scroll = { enabled = false },
+      indent = { animate = { enabled = false } },
       styles = {
         terminal = {
           wo = {
